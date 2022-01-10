b22cf330d0068889e4164d2288659b2a167f4502
54vvy <royal54vvy@gmail.com>
2022-01-10 16:24:42 +0100

    Sec commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Parasolver first Hello");
+        System.out.println("Parasolver sec Hello");
     }
 }
